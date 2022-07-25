538b58b925b88738ddbe8066765ed0173e78db1e
daimucn <laosi404@qq.com>
2022-07-25 15:50:15 +0800

    修改标题

--- Expense/ViewModel/ExpenseViewModel.swift
+++ Expense/ViewModel/ExpenseViewModel.swift
@@ -10,7 +10,7 @@ import Foundation
 class ExpenseViewModel: ObservableObject {
     @Published
     var expenseItems = [
-        ExpenseItemModel(name: "课程", type: "培训", price: "800"),
+        ExpenseItemModel(name: "课程课程", type: "培训", price: "800"),
         ExpenseItemModel(name: "MacBook", type: "数码产品", price: "4500"),
         ExpenseItemModel(name: "iPhone", type: "数码", price: "6000")
     ]
